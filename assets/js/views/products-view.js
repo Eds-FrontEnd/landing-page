db8a51ef3ed07d52879ea6197b9ba755ef4d66eb
edsonalves-dev <realcedesign@outlook.com>
2026-07-10 00:15:59 -0300

    style: adiciona estilização aos cards de produtos

--- assets/js/views/products-view.js
+++ assets/js/views/products-view.js
@@ -15,6 +15,7 @@ export async function renderProducts() {
 
   button.type = "button";
   button.textContent = "Ver mais";
+  button.className = "product-btn__plus";
 
   button.addEventListener("click", () => {
     renderNextProducts(productsContainer);
