f4433c2cb9a50327a099b9ae3aade55913e25f1d
edsonalves-dev <realcedesign@outlook.com>
2026-07-12 16:06:36 -0300

    chore: corrige nome de arquivo

--- assets/js/components/menu-component.js
+++ assets/js/components/menu-component.js
@@ -1,20 +1,30 @@
 class MenuComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <nav class="menu" aria-label="Menu principal">
-                <ul class="menu__list">
-                    <li class="menu__item">
-                        <a href="#inicio" class="menu__link">Início</a>
-                    </li>
-                    <li class="menu__item">
-                        <a href="#conceito" class="menu__link">Conceito</a>
-                    </li>
-                    <li class="menu__item">
-                        <a href="#produtos" class="menu__link">Produtos</a>
-                    </li>
-                </ul>
-            </nav>
-        `;
+      <nav class="menu" aria-label="Menu principal">
+        <ul class="menu__list">
+          <li class="menu__item">
+            <a href="#inicio" class="menu__link">Início</a>
+          </li>
+          <li class="menu__item">
+            <a href="#conceito" class="menu__link">Conceito</a>
+          </li>
+          <li class="menu__item">
+            <a href="#produtos" class="menu__link">Produtos</a>
+          </li>
+        </ul>
+
+        <a href="#carrinho" class="menu__cart" aria-label="Abrir carrinho de compras">
+          <img
+            src="assets/images/icons/cart.svg"
+            alt=""
+            width="30"
+            height="30"
+            loading="lazy"
+          />
+        </a>
+      </nav>
+    `;
   }
 }
 
