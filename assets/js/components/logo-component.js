4c13e7a26177d640718521da5d7926845346e62b
edsonalves-dev <realcedesign@outlook.com>
2026-07-18 15:48:13 -0300

    fix: ajusta caminho dinâmico do logo para múltiplas páginas

--- assets/js/components/logo-component.js
+++ assets/js/components/logo-component.js
@@ -1,9 +1,15 @@
 class LogoComponent extends HTMLElement {
   connectedCallback() {
+    const isHomePage =
+      window.location.pathname.endsWith("/") ||
+      window.location.pathname.endsWith("index.html");
+
+    const basePath = isHomePage ? "./" : "../../../";
+
     this.innerHTML = `
-      <a href="./index.html" class="header__logo">
+      <a href="${basePath}index.html" class="header__logo">
         <img
-          src="./assets/images/logo/freefashion_logo.png"
+          src="${basePath}assets/images/logo/freefashion_logo.png"
           class="logo__image"
           alt="Logo da FreeFashion"
           loading="lazy"
