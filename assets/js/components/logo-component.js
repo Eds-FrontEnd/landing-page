633b04f3a777856fd237270d40011fb89d907a97
edsonalves-dev <realcedesign@outlook.com>
2026-07-18 00:34:11 -0300

    chore: ajusta caminho de imagem da logo

--- assets/js/components/logo-component.js
+++ assets/js/components/logo-component.js
@@ -2,7 +2,7 @@ class LogoComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <a href="/" class="header__logo">
-                <img src="assets/images/logo/freefashion_logo.png" class="logo__image" alt="Logo da FreeFashion" loading="lazy"
+                <img src="../../../assets/images/logo/freefashion_logo.png" class="logo__image" alt="Logo da FreeFashion" loading="lazy"
                     width="193" height="70" />
             </a>
         `;
