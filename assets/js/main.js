8f8782c942a39d3476d17ded5f0f72154c26d72d
edsonalves-dev <realcedesign@outlook.com>
2026-07-18 15:30:43 -0300

    fix: ajusta inicialização das páginas para GitHub Pages"

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,28 @@ import "./components/cart-component.js";
 import "./components/modal-component.js";
 import "./components/whatsapp-component.js";
 
-//Pages
+// Pages
 import "./components/pages-components/home-page-component.js";
 import "./components/pages-components/register-page-component.js";
 import "./components/pages-components/contact-page-component.js";
 
-const currentPage = window.location.pathname.split("/index.html").pop();
+const currentPage = window.location.pathname.endsWith("/")
+  ? "index.html"
+  : window.location.pathname.split("/").pop();
 
 async function init() {
   try {
     switch (currentPage) {
-      case "":
       case "index.html":
+      case "":
         await import("./components/menu-component.js");
         await renderProducts();
         break;
 
-      case "register.html":
+      case "register-page.html":
+        break;
+
+      case "contact-page.html":
         break;
 
       default:
